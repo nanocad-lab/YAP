8e741e78b96b76aba2f18cba4e1f0a2cc361321d
NanoMembers <nanocad.ucla@gmail.com>
2025-09-29 16:39:32 -0700

    Update simulator_main.py

--- W2W/simulator_main.py
+++ W2W/simulator_main.py
@@ -12,7 +12,7 @@ cfg = load_modeling_config(path='configs/config.yaml',
                      debug=False)
 
 # Generate pad bitmap collection
-pad_bitmap_collection = pad_bitmap_generate_random(cfg=cfg)  
+pad_bitmap_collection = pad_bitmap_generate_random(cfg=cfg, pad_layout_pattern='center')
 
 # Explore the impact of the pitch
 particle_density_list = np.logspace(-10, -8.4, 100)
