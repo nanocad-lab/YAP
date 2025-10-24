10ed0d901693694e5f804aa65c181af777713746
Chen-Zhichao <zhichaochen@ucla.edu>
2025-10-24 13:09:44 -0700

    Adding debond stress model

--- W2W/Cu_expansion_yield_calculator.py
+++ W2W/Cu_expansion_yield_calculator.py
@@ -13,27 +13,27 @@ from debond import debond_dishing_bounds_calculator
 
 
 
-def Cu_expansion_yield_calculator(*,
+def pad_Cu_expansion_yield_map_generator(*,
         cfg,
         wafer,
         TOP_DISH_MEAN_nm: float,
         TOP_DISH_STD_nm: float,
         BOT_DISH_MEAN_nm: float,
         BOT_DISH_STD_nm: float,
-        k_et: float,
-        k_eb: float,
-        T_R: float,
-        T_anl: float,
         pad_bitmap_collection: dict,
         pad_yield_flag: bool = False,
     ):
-    num_critical_pads = pad_bitmap_collection["num_critical_pads"]
-    num_redundant_logical_pads = pad_bitmap_collection["num_redundant_logical_pads"]
-    redundant_logical_pad_copy = pad_bitmap_collection["redundant_logical_pad_copy"]
-    for die in wafer.die_list:
+    for i, die in enumerate(wafer.die_list):
         dishing_bound_array = debond_dishing_bounds_calculator(cfg, die.pad_coords) # (num_pads, 2) array: (dishing_low_nm, dishing_high_nm)
         upper_limit = - dishing_bound_array[:, 0]  # - upper Cu height limits
         lower_limit = - dishing_bound_array[:, 1]  # - lower Cu height limits
 
         pos_pad, _ = quad(lambda x: norm.pdf(x, loc=TOP_DISH_MEAN_nm + BOT_DISH_MEAN_nm, scale=np.sqrt(TOP_DISH_STD_nm**2 + BOT_DISH_STD_nm**2)), lower_limit, upper_limit)
-        # TODO: Finish the rest
+        pad_yield_map = pos_pad.reshape(die.PAD_ARR_ROW, die.PAD_ARR_COL)
+        glb_defect_pad_yield_min = min(glb_defect_pad_yield_min, np.nanmin(pad_yield_map))
+        glb_defect_pad_yield_max = max(glb_defect_pad_yield_max, np.nanmax(pad_yield_map))
+        die.pad_yield_map['Y_ce'] = pad_yield_map
+        print("Generated pad-level Cu expansion yield map for die {}.".format(i))
+    wafer.glb_pad_yield_min_max_dict['Y_ce'] = (glb_defect_pad_yield_min, glb_defect_pad_yield_max)
+    print("Global min of the pad-level Cu expansion yield: {}".format(glb_defect_pad_yield_min))
+    print("Global max of the pad-level Cu expansion yield: {}".format(glb_defect_pad_yield_max))
